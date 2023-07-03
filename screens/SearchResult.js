eb78fc9e61676e969707fd18cf6db3c997e68bbe
hfg-mikias-e <72493307+hfg-mikias-e@users.noreply.github.com>
2023-07-03 16:16:14 +0200

    Style Changes

--- screens/SearchResult.js
+++ screens/SearchResult.js
@@ -15,7 +15,7 @@ const SearchResult = () => {
   const [appIsReady, setAppIsReady] = useState(false);
   const route = useRoute();
   const title = route.params.result;
-  //console.log(title);
+  console.log(title);
   
   useEffect(() => {
     const options = {
@@ -24,7 +24,8 @@ const SearchResult = () => {
         // Request Limit lol
         //'X-RapidAPI-Key': 'fb83f7ff8emsh8a56c247160adc4p17e0edjsnbcb6d9b23ec4',
         //'X-RapidAPI-Key': '3e125506a0msh8f86510cc35398dp1d6841jsn4760cbd20cf2',
-        'X-RapidAPI-Key': '5efc68b1b8msh1c73a37cc6c191ep1bd45bjsna60ed47a68ef',
+        //'X-RapidAPI-Key': '5efc68b1b8msh1c73a37cc6c191ep1bd45bjsna60ed47a68ef',
+        'X-RapidAPI-Key': 'd8456c5235msha9b28ea7c17dcc6p1b7d88jsn534013baff7e',
         'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
       }
     };
@@ -69,6 +70,7 @@ const SearchResult = () => {
     return null;
   }
 
+  {/*
   const platformItems = streams.map(item =>
     // Platform with each Option
     <View style={styles.stream} key={streams.findIndex(e => e === item)}>
@@ -80,11 +82,12 @@ const SearchResult = () => {
       })}
     </View>
   );
+  */}
 
   const streamItems = streams.filter(index => index.watchOptions.includes("subscription")).map(item =>
     // all Platforms for the "subscription" Option
     <View key={streams.findIndex(e => e === item)}>
-      <Text style = {styles.textStream}>{item.platform}</Text>
+      <Text style = {styles.streamOption}>{item.platform}</Text>
     </View>
   );
 
@@ -101,11 +104,14 @@ const SearchResult = () => {
       <View style = {styles.page}>
         <Text style = {{ display: streams.length > 0 ? "none" : "flex" }}>Unfortunately we could not find this title on any available platforms for your country.</Text>
         
+        {/*
         {platformItems}
+        */}
 
         <View style = {styles.stream}>
-          <Text style = {styles.textStream}>Streaming on:</Text>
-          {streamItems}
+          <Text style = {styles.textStream}>Streaming available on:</Text>
+          <View style={{gap: 12}}>{streamItems}</View>
+          <Text style={{width: "100%", opacity: 0.6, textAlign: "center"}}>you might need an active subscription for the listed platforms to be able to access this movie</Text>
         </View>
 
         {/*
